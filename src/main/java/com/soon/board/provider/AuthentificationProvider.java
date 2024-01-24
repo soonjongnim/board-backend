b7dc1cb9d96d12e3b6b608d32b9d601be4bebaeb
soon9086 <soonjongnim@naver.com>
2024-01-24 14:22:35 +0900

    클래스파일과 config관련 경로 설정 확인

--- src/main/java/com/soon/board/provider/AuthentificationProvider.java
+++ src/main/java/com/soon/board/provider/AuthentificationProvider.java
@@ -14,6 +14,7 @@ import com.oracle.bmc.auth.SimplePrivateKeySupplier;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URL;
 
 @Service
 public class AuthentificationProvider {
@@ -21,6 +22,14 @@ public class AuthentificationProvider {
     public AuthenticationDetailsProvider getAuthenticationDetailsProvider() throws IOException {
 
         ClassLoader classLoader = AuthentificationProvider.class.getClassLoader();
+        URL resourceUrl = classLoader.getResource("config");
+
+        if (resourceUrl != null) {
+            String filePath = new File(resourceUrl.getFile()).getPath();
+            System.out.println("File Path: " + filePath);
+        } else {
+            System.err.println("Resource not found");
+        }
         File tempConfigFile = new File(classLoader.getResource("config").getFile());
         File tempOCIAPIKey = new File(classLoader.getResource("oci_api_key.pem").getFile());
 
