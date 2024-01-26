650bf354a2f4f9bbfc576dd7fd8af497ab8a17f2
soon9086 <soonjongnim@naver.com>
2024-01-26 18:59:32 +0900

    오라클 이미지관련 업로드 설정, 서버용 적용완료!

--- src/main/java/com/soon/board/provider/AuthentificationProvider.java
+++ src/main/java/com/soon/board/provider/AuthentificationProvider.java
@@ -1,7 +1,6 @@
 package com.soon.board.provider;
 
 import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -13,7 +12,6 @@ import org.springframework.stereotype.Service;
 import com.oracle.bmc.Region;
 import com.oracle.bmc.auth.AuthenticationDetailsProvider;
 import com.oracle.bmc.auth.SimpleAuthenticationDetailsProvider;
-import com.oracle.bmc.auth.SimplePrivateKeySupplier;
 
 @Service
 public class AuthentificationProvider {
@@ -21,7 +19,7 @@ public class AuthentificationProvider {
     public AuthenticationDetailsProvider getAuthenticationDetailsProvider() throws IOException {
 
         ClassLoader classLoader = AuthentificationProvider.class.getClassLoader();
-        InputStream tempConfigInputStream = classLoader.getResourceAsStream("BOOT-INF/classes/config"); // server용 클래스패스
+        InputStream tempConfigInputStream = classLoader.getResourceAsStream("BOOT-INF/classes/config"); // server용
 
         // server용  //
         Properties properties = new Properties();
@@ -46,9 +44,8 @@ public class AuthentificationProvider {
 //        File tempOCIAPIKey = new File(classLoader.getResource("oci_api_key.pem").getFile()); // localhost용
 
 //        ConfigFile config = ConfigFileReader.parse(tempConfigFile.getPath(), "DEFAULT"); // localhost용
-//        ClassLoader classLoader2 = getClass().getClassLoader();
-        Supplier<InputStream> privateKeySupplier = () -> classLoader.getResourceAsStream("BOOT-INF/classes/oci_api_key.pem");
 //        Supplier<InputStream> privateKeySupplier = new SimplePrivateKeySupplier(tempOCIAPIKey.getPath());	// localhost용
+        Supplier<InputStream> privateKeySupplier = () -> classLoader.getResourceAsStream("BOOT-INF/classes/oci_api_key.pem");	// server용
 
         AuthenticationDetailsProvider provider = SimpleAuthenticationDetailsProvider.builder()
                 .tenantId(properties.getProperty("tenancy")).userId(properties.getProperty("user")).fingerprint(properties.getProperty("fingerprint"))
